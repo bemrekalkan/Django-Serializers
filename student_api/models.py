3316bae2b0ac53a7727ba9bbee9303ac2856da24
bemrekalkan <bemre.kalkan@gmail.com>
2022-09-17 14:13:00 +0300

    .

--- student_api/models.py
+++ student_api/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
+# Create your models here.
 class Path(models.Model):
     path_name = models.CharField(max_length=50)
 
     def __str__(self):
         return f"{self.path_name}"
+
 class Student(models.Model):
     path = models.ForeignKey(Path, related_name='students', on_delete=models.CASCADE)
     first_name = models.CharField(max_length=30)
--- student_api/serializers.py
+++ student_api/serializers.py
@@ -1,31 +1,40 @@
 from rest_framework import serializers
-from .models import Student
+from .models import Path, Student
+
+# 1.yöntem
+# class StudentSerializer(serializers.Serializer):
+#     first_name = serializers.CharField(max_length=30)
+#     last_name = serializers.CharField(max_length=30)
+#     number = serializers.IntegerField(required=False)  # null true olanlar için required false vermemiz gerekiyor.
+    
+#     def create(self, validated_data):
+#         return Student.objects.create(**validated_data)
+
+#     def update(self, instance, validated_data):
+#         instance.first_name = validated_data.get('first_name', instance.first_name)
+#         instance.last_name = validated_data.get('last_name', instance.last_name)
+#         instance.number = validated_data.get('number', instance.number)
+#         instance.save()
+#         return instance
 
 
-#! 1. METHOD 👇
-""" class StudentSerializer(serializers.Serializer):
-    first_name = serializers.CharField(max_length=30)
-    last_name = serializers.CharField(max_length=30)
-    number = serializers.IntegerField(required=False)
+#2. yöntem
 
-    def create(self, validated_data):
-        return Student.objects.create(**validated_data)
-        #? '**' 👉 kwarg: Dict yapısında gelen veriyi açıyor
+class PathSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Path
+        fields = ["id", "path_name"]
 
-    def update(self, instance, validated_data):
-        instance.first_name = validated_data.get('first_name', instance.first_name)
-        instance.last_name = validated_data.get('last_name', instance.last_name)
-        instance.number = validated_data.get('number', instance.number)
-        instance.save()
-        return instance """
 
-#! 2. METHOD 👇 SHORT ONE 💨
 class StudentSerializer(serializers.ModelSerializer):
-    full_name = serializers.SerializerMethodField()
-    def get_full_name(self, obj):
-        return f'{obj.first_name} {obj.last_name}'
+
+    # full_name = serializers.SerializerMethodField()
+    # def get_full_name(self, obj):
+    #     return f'{obj.first_name} - {obj.last_name}'
+    path = serializers.StringRelatedField()
+    path_id = serializers.IntegerField()
     class Meta:
         model = Student
-        fields = ["id", "full_name", "first_name", "last_name", "number"]
-        # fields = '__all__'
-        # exclude = ['number']
+        fields = ["id", "path_id", "path", "first_name", "last_name", "number"]
+        # fields = '__all__' # hepsini getir
+        # exclude = ['number']  # numbers haricini getir.
